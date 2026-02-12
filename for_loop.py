493b0b6b3799d545d0829ba071ffb6c2774fe9ae
Md Sazzatul Miraz <mirazbakul1997@gmail.com>
2026-02-12 05:00:44 -1200

    Add for_loop

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "Mango", "lichi", "Jackfruit", "Orange", "Banana", "Berry", "kiwwi",]
+# for fruit in fruits:
+#     print(fruit)
+for fruit in fruits:
+    if fruit == "Banana":
+        print("Found Banana")
+    else:
+        print("Not found")
